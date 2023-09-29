2293fa49a9642ef4553e2f4ed26e656f9b5e0d24
ahmadhere-jpg <142297605+ahmadhere-jpg@users.noreply.github.com>
2023-09-30 02:10:02 +0500

    backendems

---
+++ src/controllers/employee.ts
@@ -0,0 +1,86 @@
+import { Employee } from '../model/employee';
+import { Employment } from '../model/employment';
+import { Attendance } from '../model/attendance';
+import { Role } from '../model/role';
+import express from 'express';
+import authenticateToken from '../middleware/auth';
+
+const router = express.Router();
+// router.post('/new', authenticateToken ,async (req: express.Request, res: express.Response) => {
+
+router.post('/new', async (req: express.Request, res: express.Response) => {
+    try {
+        const createdEmployee = await Employee.create(req.body);
+        res.status(201).json({ message: "Employee Added Successfully", employee: createdEmployee });
+    } catch (error) {
+        console.error('Error adding employee:', error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+// Read all employees
+router.get('/all', async (req: express.Request, res: express.Response) => {
+    try {
+        const employees = await Employee.find();
+        res.json(employees);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Read a specific employee
+router.get('/:id', async (req: express.Request, res: express.Response) => {
+    try {
+        const employee = await Employee.findById(req.params.id);
+        if (!employee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        res.json(employee);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Update an employee
+router.patch('/update_employees/:id', async (req: express.Request, res: express.Response) => {
+    try {
+        const EmployeeId = req.params.id;
+        const updatedFields = req.body; 
+
+        const updatedEmployeeFields: any = {};
+        for (const key in updatedFields) {
+            updatedEmployeeFields[key] = updatedFields[key];
+        }
+
+        const updatedEmployee = await Employee.findByIdAndUpdate(EmployeeId, updatedEmployeeFields, { new: true });
+
+        if (!updatedEmployee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+
+        res.json(updatedEmployee);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+// Delete an employee
+router.delete('/delete_employees/:id', async (req: express.Request, res: express.Response) => {
+    try {
+        const employeeId = req.params.id;
+        const deletedEmployee = await Employee.findByIdAndRemove(employeeId);
+        if (!deletedEmployee) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        const delteemployment = await Employment.findByIdAndRemove(employeeId);
+        const deleteRole = await Role.findByIdAndRemove(employeeId);
+        const deleteAttendance = await Attendance.findByIdAndRemove(employeeId);
+        res.json({ message: 'Employee deleted' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
